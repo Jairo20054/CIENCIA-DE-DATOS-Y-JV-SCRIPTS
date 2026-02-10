177146ef686ed2eb45fa796c2dfa8ba37e900ef8
Jairo20054 <castillojairo200@gmail.com>
2026-02-10 02:02:16 -0500

    CAMBIOS

--- Python_Datos/Datos.py
+++ Python_Datos/Datos.py
@@ -1,29 +1,9 @@
 import numpy as np
 import pandas as pd  # <--- ¡Esta es la línea que faltaba!
 
-# 1. Definimos el diccionario
-datos_dict = {
-    'ID_Pedido': [101, 102, 103, 104, 105, 106, 107, 108, 109, 110],
-    'Fecha': ['2024-01-01', '2024-01-02', '2024-01-02', '2024-01-03', '2024-01-04', 
-              '2024-01-05', '2024-01-05', '2024-01-06', '2024-01-07', '2024-01-08'],
-    'Producto': ['Laptop', 'Mouse', 'Monitor', 'Laptop', 'Teclado', 'Mouse', 'Monitor', 'Laptop', 'Teclado', 'Smartwatch'],
-    'Categoria': ['Hardware', 'Accesorio', 'Hardware', 'Hardware', 'Accesorio', 'Accesorio', 'Hardware', 'Hardware', 'Accesorio', 'Gadget'],
-    'Precio_USD': [1200, 25, 300, 1200, 50, 25, 300, 1200, 50, 150],
-    'Cantidad': [1, 5, 2, 1, 3, np.nan, 1, 2, np.nan, 1],
-    'Ciudad': ['CDMX', 'Bogotá', 'Madrid', 'CDMX', 'Lima', 'Bogotá', 'Madrid', 'Santiago', 'Lima', 'CDMX']
-}
+##
 
-# 2. Creamos el DataFrame
-df = pd.DataFrame(datos_dict)
-
-# 3. Ver la forma y el resultado
-forma = df.shape
-conversión = df.astype(float)
-print(f"La forma de la matriz es: {forma}")
-print(df.info()) # sirve para saber los tipos de daos 
-print(conversión)
-
-# Bases de dtaos (base para repasar)
+import pandas as pd
 bases_de_dtaos = {
     "ID_Cliente": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     "Nombre": ["Ana", "Luis", "Marta", "Jose", "Sofia", "Carlos", "Diana", "Pedro", "Lucia", "Andres"],
@@ -36,6 +16,10 @@ bases_de_dtaos = {
               "2024-02-06", "2024-02-07", "2024-02-08", "2024-02-09", "2024-02-10"],
 }
 
-df_bases_de_dtaos = pd.DataFrame(bases_de_dtaos)
-print(df_bases_de_dtaos.head())
+df = pd.DataFrame(bases_de_dtaos)
+#df[['ID_Cliente', 'Precio_USD']]= df[['ID_Cliente', 'Precio_USD']]
+##print(df[['ID_Cliente', 'Precio_USD']].astype(float))
+base = bases_de_dtaos.loc[[1,2]]
+print(base)## sirve para sacar el promedio, desviación estar, min y maximo
+
 
