5c8c965d74f2d16dd22f531a08bc1afb3d42ed21
Jairo20054 <castillojairo2001@gmail.com>
2026-06-26 21:27:28 +0000

    ejercicio

--- Javascripts/Soluciones ejercicios.js
+++ Javascripts/Soluciones ejercicios.js
@@ -8,6 +8,24 @@ console.log(segundo);
 2// Crea una variable con tu edad y muestra el mensaje: “Tengo X años”.
 
 const edad = 25; 
+const nombres = "Castillo"
 
 console.log(`tengo ${edad} años`); 
 console.log("tengo " + edad + " años");
+console.log("mi nombre es " + nombres + "y tengo" + edad + "años");
+
+3// Crea dos números y muestra en consola la suma.
+
+const numero1 = 20;
+
+const numero2 = 40;
+
+console.log(numero1 + numero2);
+console.log(numero2 + numero1);
+
+4// Crea una función que reciba dos números y retorne su suma.
+
+const función = (numero4, numero3) => {
+    return numero4 + numero3;
+}
+console.log(función(15, 25));
