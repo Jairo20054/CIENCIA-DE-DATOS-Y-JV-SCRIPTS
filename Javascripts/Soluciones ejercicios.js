674e930c44c8a9a389fbb09a86d66bf0c58cb614
Jairo20054 <castillojairo2001@gmail.com>
2026-06-26 20:39:32 +0000

    Actualización

--- Javascripts/Soluciones ejercicios.js
+++ Javascripts/Soluciones ejercicios.js
@@ -1,2 +1,13 @@
-1. Crea una variable con tu nombre y muéstrala en consola.
-const
+1.//Crea una variable con tu nombre y muéstrala en consola.
+const nombre = "Jairo";
+console.log(nombre);
+
+const segundo = "Andres";
+console.log(segundo);
+
+2// Crea una variable con tu edad y muestra el mensaje: “Tengo X años”.
+
+const edad = 25; 
+
+console.log(`tengo ${edad} años`); 
+console.log("tengo " + edad + " años");
